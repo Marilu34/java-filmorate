3c7e48c266107a0000bbc929a792c2af9bec5eb8
Marilu34 <lulima@yandex.ru>
2023-05-08 19:57:29 +0300

    1

--- src/main/java/org/example/service/FilmService.java
+++ src/main/java/org/example/service/FilmService.java
@@ -4,6 +4,7 @@ import org.example.model.Film;
 import org.example.storage.film.FilmStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 
 
@@ -13,19 +14,19 @@ public class FilmService {
     private final FilmStorage filmStorage;
 
     @Autowired
-    public FilmService(FilmStorage filmStorage){
+    public FilmService(FilmStorage filmStorage) {
         this.filmStorage = filmStorage;
     }
 
-    public ArrayList<Film> getAll(){
+    public ArrayList<Film> getAll() {
         return filmStorage.getAllFilms();
     }
 
-    public Film add(Film film){
+    public Film add(Film film) {
         return filmStorage.createFilm(film);
     }
 
-    public Film update(Film film){
+    public Film update(Film film) {
         return filmStorage.updateFilm(film);
     }
 
@@ -33,7 +34,7 @@ public class FilmService {
         filmStorage.addLike(filmId, userId);
     }
 
-    public void deleteLike(int filmId, int userId){
+    public void deleteLike(int filmId, int userId) {
         filmStorage.deleteLike(filmId, userId);
     }
 
