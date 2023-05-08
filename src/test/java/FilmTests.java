f9c8315729f5ca7e2a42ed7470d24c97f8b86ea2
Marilu34 <lulima@yandex.ru>
2023-05-08 20:12:52 +0300

    1

--- src/test/java/FilmTests.java
+++ src/test/java/FilmTests.java
@@ -3,12 +3,11 @@ import org.example.storage.film.InMemoryFilmStorage;
 import org.example.exceptions.ValidationException;
 import org.example.model.Film;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
@@ -16,8 +15,6 @@ import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
